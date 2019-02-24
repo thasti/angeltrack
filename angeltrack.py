1f35bc1a6c7affd41645d829296f04ccb8ce2dda
Stefan Biereigel <stefan@biereigel.de>
2019-02-24 17:13:12 +0100

    make TX offset a command line argument

--- angeltrack.py
+++ angeltrack.py
@@ -6,12 +6,11 @@ import argparse
 api_url_rx = "http://127.0.0.1:8091/sdrangel/deviceset/0/channel/0/settings"
 api_url_tx = "http://127.0.0.1:8091/sdrangel/deviceset/1/channel/0/settings"
 
-tx_offset = -100e3
-
 parser = argparse.ArgumentParser(description="RX/TX control for sdrangel")
 arg_group = parser.add_mutually_exclusive_group(required=True)
 arg_group.add_argument('--rx', action='store_true')
 arg_group.add_argument('--tx', action='store_true')
+parser.add_argument('--txoffs', type=float, default=0, help='Specify offset added to RX frequency. TX = RX + TXOFFS')
 args = parser.parse_args()
 
 # query demodulator properties
@@ -23,7 +22,7 @@ tx_config_req = requests.get(api_url_tx)
 tx_config = tx_config_req.json()
 
 if args.tx:
-    tx_config['SSBModSettings']['inputFrequencyOffset'] = rx_config['SSBDemodSettings']['inputFrequencyOffset'] + tx_offset
+    tx_config['SSBModSettings']['inputFrequencyOffset'] = rx_config['SSBDemodSettings']['inputFrequencyOffset'] + args.txoffs
     rx_config['SSBDemodSettings']['audioMute'] = 1
     tx_config['SSBModSettings']['audioMute'] = 0
 
